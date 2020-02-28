313b4f43dc99c569ee2c2be7a06ef458e11a50c3
zain Ahmed <zainahmed0597@gmail.com>
2020-02-28 22:41:30 +0500

    First commit

---
+++ 40 - sizeof.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a;
+	char b;
+	double c;
+	double array[10];
+	 
+	cout <<  sizeof(a) << endl;
+	cout <<  sizeof(b) << endl;
+	cout <<  sizeof(c) << endl;
+	cout <<  sizeof(array) << endl;
+	cout <<  sizeof(array) / sizeof(array[0]) << endl;
+
+
+}
